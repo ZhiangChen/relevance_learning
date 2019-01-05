6838a5f9028901ba734bcad20330625e5d74f4cb
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2019-01-04 23:30:00 -0500

    debugging

--- main.py
+++ main.py
@@ -76,10 +76,11 @@ else:
 
 model = unet(num_classes=3, seg_only=True)
 model_checkpoint = ModelCheckpoint(args.logs_dir, monitor='loss',verbose=1, save_best_only=True)
-tb = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True,
-                            write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None,
-                            embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
+# tb = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True,
+#                             write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None,
+#                             embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
 for i in range(args.num_epochs):
+  print(i)
   model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
   eval = model.evaluate_generator(myValGene, steps=10, verbose=1)
   print(eval)
