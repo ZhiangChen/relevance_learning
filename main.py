0827d6a19ff7c2133d86303c3439f0af0a268d6e
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2019-01-06 16:32:36 -0500

    test and predict

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from keras.callbacks import ModelCheckpoint, TensorBoard
 import argparse
 import os
 from model.data_loading import *
+import numpy as np
 
 ap = argparse.ArgumentParser()
 
 ap.add_argument('-p', '--use_pfile', default=False, dest='use_pfile', help='use predefined parameter file',  type=bool)
-ap.add_argument('-m', '--mode', default='train', choices=['train', 'test'])
+ap.add_argument('-m', '--mode', default='train', choices=['train', 'predict', 'test'])
 ap.add_argument('-l', '--logs_dir', default='unet_atlanta.hdf5')
 ap.add_argument('-b', '--batch_size', default=2, dest='batch_size', type=int)
 ap.add_argument('-n', '--num_epochs', default=10, dest='num_epochs', type=int)
@@ -45,59 +46,81 @@ target_size = (256, 256)
 # trainGenerator(batch_size, train_path, image_folder, mask_folder, heatmap_folder, aug_dict, image_color_mode="rgb",
 #                    mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",heatmap_save_prefix="heatmap",
 #                    flag_multi_class=True, num_class=2, save_to_dir=None, target_size=(256, 256), seed=1)
-if not args.use_pfile:
-  print(args.batch_size, args.image_folder, args.train_path, data_gen_args)
-  # myGene = trainGenerator(batch_size=args.batch_size, train_path=args.train_path, image_folder=args.image_folder, mask_folder=args.mask_folder,
-  #                       heatmap_folder=args.heatmap_folder, aug_dict=data_gen_args, image_color_mode="rgb",
-  #                  mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask", heatmap_save_prefix="heatmap",
-  #                  flag_multi_class=True, num_class=args.class_num, save_to_dir=None, target_size=(args.h, args.w), seed=1)
-  myGene = trainGenerator(args.batch_size, args.train_path, args.image_folder,
-                          args.mask_folder,
-                          args.heatmap_folder, data_gen_args, image_color_mode="rgb",
-                          mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
-                          heatmap_save_prefix="heatmap",
-                          flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
-                          target_size=(args.h, args.w), seed=1)
-  # myValGene = trainGenerator(batch_size=args.batch_size, train_path=args.valid_path, image_folder=args.image_folder,
-  #                         mask_folder=args.mask_folder,
-  #                         heatmap_folder=args.heatmap_folder, aug_dict=data_gen_args, image_color_mode="rgb",
-  #                         mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
-  #                         heatmap_save_prefix="heatmap",
-  #                         flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
-  #                         target_size=(args.h, args.w), seed=1)
-  myValGene = trainGenerator(args.batch_size, args.valid_path, args.image_folder,
-                             args.mask_folder,
-                             args.heatmap_folder, data_gen_args, image_color_mode="rgb",
-                             mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
-                             heatmap_save_prefix="heatmap",
-                             flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
-                             target_size=(args.h, args.w), seed=1)
-else:
-  myGene = trainGenerator(batch_size, train_path, image_folder, mask_folder, heatmap_folder, data_gen_args, image_color_mode="rgb",
-                   mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask", heatmap_save_prefix="heatmap",
-                   flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
-  myValGene = trainGenerator(batch_size, valid_path, image_folder, mask_folder, heatmap_folder, data_gen_args,
-                          image_color_mode="rgb",
-                          mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
-                          heatmap_save_prefix="heatmap",
-                          flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
+if args.mode == 'train':
+  if not args.use_pfile:
+    myGene = trainGenerator(args.batch_size, args.train_path, args.image_folder,
+                            args.mask_folder,
+                            args.heatmap_folder, data_gen_args, image_color_mode="rgb",
+                            mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                            heatmap_save_prefix="heatmap",
+                            flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
+                            target_size=(args.h, args.w), seed=1)
+    myValGene = trainGenerator(args.batch_size, args.valid_path, args.image_folder,
+                               args.mask_folder,
+                               args.heatmap_folder, data_gen_args, image_color_mode="rgb",
+                               mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                               heatmap_save_prefix="heatmap",
+                               flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
+                               target_size=(args.h, args.w), seed=1)
+  else:
+    myGene = trainGenerator(batch_size, train_path, image_folder, mask_folder, heatmap_folder, data_gen_args, image_color_mode="rgb",
+                     mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask", heatmap_save_prefix="heatmap",
+                     flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
+    myValGene = trainGenerator(batch_size, valid_path, image_folder, mask_folder, heatmap_folder, data_gen_args,
+                            image_color_mode="rgb",
+                            mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                            heatmap_save_prefix="heatmap",
+                            flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
 
-model = unet(num_classes=3, seg_only=True)
-model_checkpoint = ModelCheckpoint(args.logs_dir, monitor='loss',verbose=1, save_best_only=True)
-# tb = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True,
-#                             write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None,
-#                             embeddings_metadata=None, embeddings_data=None)
-tb = TensorBoard(log_dir = './logs', histogram_freq = 0,
-  batch_size = 16, write_graph = True, write_grads = False,
-  write_images = False, embeddings_freq = 10,
-  embeddings_layer_names = ['input', 'segmentation', 'heatmap'], embeddings_metadata = None,
-  embeddings_data = None, update_freq = 160)
-# for i in range(args.num_epochs):
-model.fit_generator(myGene,steps_per_epoch=300,epochs=args.num_epochs, validation_data=myValGene, validation_steps=300, callbacks=[model_checkpoint, tb])
-eval = model.evaluate_generator(myValGene, steps=10, verbose=1)
-print(eval)
-model2 = unet(pretrained_weights=args.logs_dir, num_classes=3, seg_only=False)
-# for j in range(args.num_epochs):
-model2.fit_generator(myGene,steps_per_epoch=300,epochs=args.num_epochs, validation_data=myValGene, validation_steps=300,callbacks=[model_checkpoint, tb])
-eval = model2.evaluate_generator(myValGene, steps=10, verbose=1)
-print(eval)
+  try:
+    model = unet(num_classes=3, seg_only=True, pretrained_weights=args.logs_dir)
+  except:
+    model = unet(num_classes=3, seg_only=True)
+  model_checkpoint = ModelCheckpoint(args.logs_dir, monitor='loss',verbose=1, save_best_only=True)
+  tb = TensorBoard(log_dir = './logs', histogram_freq = 0,
+    batch_size = 16, write_graph = True, write_grads = False,
+    write_images = False, embeddings_freq = 0,
+    embeddings_layer_names = None, embeddings_metadata = None,
+    embeddings_data = None, update_freq = 160)
+  # for i in range(args.num_epochs):
+  model.fit_generator(myGene,steps_per_epoch=300,epochs=args.num_epochs, validation_data=myValGene, validation_steps=300, callbacks=[model_checkpoint, tb])
+  eval = model.evaluate_generator(myValGene, steps=10, verbose=1)
+  print(eval)
+  model2 = unet(pretrained_weights=args.logs_dir, num_classes=3, seg_only=False)
+  # for j in range(args.num_epochs):
+  model2.fit_generator(myGene,steps_per_epoch=300,epochs=args.num_epochs, validation_data=myValGene, validation_steps=300,callbacks=[model_checkpoint, tb])
+  eval = model2.evaluate_generator(myValGene, steps=10, verbose=1)
+  print(eval)
+
+elif args.mode == 'test':
+  if not args.use_pfile:
+    myGene = trainGenerator(args.batch_size, args.test_path, args.image_folder,
+                            args.mask_folder,
+                            args.heatmap_folder, data_gen_args, image_color_mode="rgb",
+                            mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                            heatmap_save_prefix="heatmap",
+                            flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
+                            target_size=(args.h, args.w), seed=1)
+  else:
+    myGene = trainGenerator(batch_size, test_path, image_folder, mask_folder, heatmap_folder, data_gen_args, image_color_mode="rgb",
+                     mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask", heatmap_save_prefix="heatmap",
+                     flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
+
+  try:
+    model = unet(num_classes=3, seg_only=True, pretrained_weights=args.logs_dir)
+  except:
+    raise ValueError('No model at specified dir')
+  model_checkpoint = ModelCheckpoint(args.logs_dir, monitor='loss', verbose=1, save_best_only=True)
+  eval = model.evaluate_generator(myGene, verbose=1)
+
+else:
+  if not args.use_pfile:
+    myGene = testGenerator(args.test_path)
+  else:
+    myGene = testGenerator(test_path)
+  try:
+    model = unet(num_classes=3, seg_only=True, pretrained_weights=args.logs_dir)
+  except:
+    raise ValueError('No model at specified dir')
+  output = model.predict_generator(myGene)
+  np.savez('prediction.npz', output)
