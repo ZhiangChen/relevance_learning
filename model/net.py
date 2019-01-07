a3dcee5ac74a3305c597c10277a969392ca5791e
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2019-01-06 20:52:16 -0500

    weight loading

--- model/net.py
+++ model/net.py
@@ -82,6 +82,7 @@ def unet(pretrained_weights=None, input_size=(256, 256, 3), num_classes=2, seg_o
   # model.summary()
 
   if (pretrained_weights):
+    print('loading weights'+pretrained_weights)
     model.load_weights(pretrained_weights)
 
   return model
