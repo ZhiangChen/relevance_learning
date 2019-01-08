1a8c383b4a007ea8f30e2a974aa3393b47ef7a67
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2019-01-07 22:26:13 -0500

    use keras generator for prediction

--- model/data_loading.py
+++ model/data_loading.py
@@ -105,12 +105,39 @@ def trainGenerator(batch_size, train_path, image_folder, mask_folder, heatmap_fo
     # yield (img, [mask, heatmap])
     yield (img, [mask, heatmap])
 
-@threadsafe_generator
-def testGenerator(test_path, num_image=5, target_size=(256, 256), flag_multi_class=False, as_gray=False):
-    for i in range(num_image):
-      img = io.imread(os.path.join(test_path, "%d.png" % i), as_gray=as_gray)
+
+# def testGenerator(test_path, num_image=5, target_size=(256, 256), flag_multi_class=False, as_gray=False):
+
+    # for i in range(num_image):
+    #   img = io.imread(os.path.join(test_path, "%d.png" % i), as_gray=as_gray)
+    #   img = img / 255.0
+    #   img = trans.resize(img, target_size)
+    #   img = np.reshape(img, img.shape + (1,)) if (not flag_multi_class) else img
+    #   img = np.reshape(img, (1,) + img.shape)
+    #   yield img
+
+def testGenerator(batch_size, train_path, image_folder, image_color_mode="rgb",
+                   image_save_prefix="image",
+                   save_to_dir=None, target_size=(256, 256), seed=1):
+  '''
+  can generate image and mask at the same time
+  use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
+  if you want to visualize the results of generator, set save_to_dir = "your path"
+  '''
+  image_datagen = ImageDataGenerator()
+  image_generator = image_datagen.flow_from_directory(
+    train_path,
+    classes=[image_folder],
+    class_mode=None,
+    color_mode=image_color_mode,
+    target_size=target_size,
+    batch_size=batch_size,
+    save_to_dir=save_to_dir,
+    save_prefix=image_save_prefix,
+    seed=seed)
+  # print('trying to adjust data')
+  for img in image_generator:
+    if np.max(img) > 1:
       img = img / 255.0
-      img = trans.resize(img, target_size)
-      img = np.reshape(img, img.shape + (1,)) if (not flag_multi_class) else img
-      img = np.reshape(img, (1,) + img.shape)
-      yield img
+    # yield (img, [mask, heatmap])
+    yield img
