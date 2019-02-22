f97da603b92d6994b5e26e4e09bcf73d8b2883cb
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2019-02-22 15:08:44 -0500

    another loss?

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ if args.mode == 'train':
   if not args.use_pfile:
     model2.fit_generator(myGene,steps_per_epoch=300,epochs=args.num_epochs, validation_data=myValGene, validation_steps=5,callbacks=[model_checkpoint, tb])
   else:
-    model2.fit_generator(myGene,steps_per_epoch=300,epochs=5 * num_epochs, validation_data=myValGene, validation_steps=5,callbacks=[model_checkpoint, tb])
+    model2.fit_generator(myGene,steps_per_epoch=300,epochs=num_epochs, validation_data=myValGene, validation_steps=5,callbacks=[model_checkpoint, tb])
 
 elif args.mode == 'test':
   if not args.use_pfile:
