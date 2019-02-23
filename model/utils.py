56797a5705da433208a3c8505d4a0ff7e4f22c86
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2019-02-23 17:26:48 -0500

    change loss

--- model/utils.py
+++ model/utils.py
@@ -6,7 +6,7 @@ import os
 import tensorflow as tf
 
 def mse(y_true, y_pred):
-  return K.mean(K.square(y_pred - y_true))
+  return K.sum(K.square(y_pred - y_true))
 
 
 def focal(y_true, y_pred, gamma=2., alpha=.25):
@@ -26,7 +26,7 @@ def focal_L2(y_true, y_pred, gamma=2., alpha=.75):
 
 
 def weighted_L2(y_true, y_pred):
-  return K.mean(K.exp(K.clip(y_true, 0, 1) - K.clip(y_pred, 0, 1)) * K.square(y_pred - y_true))
+  return K.sum(tf.multiply(K.exp(K.clip(y_true, 0, 1) - K.clip(y_pred, 0, 1)), K.square(y_pred - y_true)))
 
 
 def save_images(net_out, save_dir, visualize=False):
