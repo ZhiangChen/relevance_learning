d6f1f7b987efd765ceb28a7b8f49846091959600
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2019-02-25 23:52:24 -0500

    baseline network

---
+++ baseline.py
@@ -0,0 +1,142 @@
+from model.net import unet, regnet
+from keras.callbacks import ModelCheckpoint, TensorBoard
+import argparse
+import os
+import glob
+from model.data_loading import *
+from model.utils import save_images
+import numpy as np
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument('-p', '--use_pfile', default=False, dest='use_pfile', help='use predefined parameter file',  type=bool)
+ap.add_argument('-m', '--mode', default='train', choices=['train', 'predict', 'test'])
+ap.add_argument('-l', '--logs_dir', default='unet_atlanta.hdf5')
+ap.add_argument('-b', '--batch_size', default=2, dest='batch_size', type=int)
+ap.add_argument('-n', '--num_epochs', default=10, dest='num_epochs', type=int)
+ap.add_argument('-tr', '--train_path', default='./training_data', dest='train_path')
+ap.add_argument('-va', '--valid_path', default='./validation_data', dest='valid_path')
+ap.add_argument('-te', '--test_path', default='./test_data', dest='test_path')
+ap.add_argument('-if', '--image_folder', default='images', dest='image_folder')
+ap.add_argument('-mf', '--mask_folder', default='masks', dest='mask_folder')
+ap.add_argument('-hf', '--heatmap_folder', default='heatmaps', dest='heatmap_folder')
+ap.add_argument('-ht', '--target_height', default=256, dest='h', type=int)
+ap.add_argument('-wd', '--target_width', default=256, dest='w', type=int)
+ap.add_argument('-is', '--image_save_prefix', default='image', dest='image_save_prefix')
+ap.add_argument('-ms', '--mask_save_prefix', default='mask', dest='mask_save_prefix')
+ap.add_argument('-hs', '--heatmap_save_prefix', default='heatmap', dest='heatmap_save_prefix')
+ap.add_argument('-c', '--class_num', default=2, dest='class_num', type=int)
+ap.add_argument('-s', '--save_dir', default='../results', dest='save_dir')
+args = ap.parse_args()
+
+# data_gen_args = dict(horizontal_flip=True,)
+data_gen_args = dict(rotation_range=30,
+                    width_shift_range=0.05,
+                    height_shift_range=0.05,
+                    shear_range=0.05,
+                    zoom_range=0.05,
+                    horizontal_flip=True,
+                    fill_mode='reflect')
+batch_size = 16
+train_path = '../scale6_train/multiclass/train_18_new/training'
+valid_path = '../scale6_train/multiclass/train_18_new/validation'
+test_path = '../scale6_train/multiclass/train_18_new/validation'
+num_epochs = 20
+# train_path = 'M:/data/multiclass/heatmap_train/train/training'
+# valid_path = 'M:/data/scale6_train/multiclass/heatmap_train/train/validation'
+# test_path = 'M:/data/scale6_train/multiclass/heatmap_train/train/validation'
+image_folder = 'images'
+mask_folder = 'masks'
+heatmap_folder = 'heatmaps'
+class_num = 4
+target_size = (256, 256)
+# trainGenerator(batch_size, train_path, image_folder, mask_folder, heatmap_folder, aug_dict, image_color_mode="rgb",
+#                    mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",heatmap_save_prefix="heatmap",
+#                    flag_multi_class=True, num_class=2, save_to_dir=None, target_size=(256, 256), seed=1)
+if args.mode == 'train':
+  if not args.use_pfile:
+    myGene = trainGenerator(args.batch_size, args.train_path, args.image_folder,
+                            args.mask_folder,
+                            args.heatmap_folder, data_gen_args, image_color_mode="rgb",
+                            mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                            heatmap_save_prefix="heatmap",
+                            flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
+                            target_size=(args.h, args.w), seed=1)
+    myValGene = trainGenerator(args.batch_size, args.valid_path, args.image_folder,
+                               args.mask_folder,
+                               args.heatmap_folder, data_gen_args, image_color_mode="rgb",
+                               mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                               heatmap_save_prefix="heatmap",
+                               flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
+                               target_size=(args.h, args.w), seed=1)
+  else:
+    myGene = trainGenerator(batch_size, train_path, image_folder, mask_folder, heatmap_folder, data_gen_args, image_color_mode="rgb",
+                     mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask", heatmap_save_prefix="heatmap",
+                     flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
+    myValGene = trainGenerator(batch_size, valid_path, image_folder, mask_folder, heatmap_folder, data_gen_args,
+                            image_color_mode="rgb",
+                            mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                            heatmap_save_prefix="heatmap",
+                            flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
+  print('here')
+
+  model_checkpoint = ModelCheckpoint(args.logs_dir, monitor='loss',verbose=1, save_best_only=False)
+  model_checkpoint_reg = ModelCheckpoint('reg_' + args.logs_dir, monitor='loss', verbose=1, save_best_only=False)
+  tb = TensorBoard(log_dir = './logs', histogram_freq = 0,
+    batch_size = 16, write_graph = True, write_grads = False,
+    write_images = False, embeddings_freq = 0,
+    embeddings_layer_names = None, embeddings_metadata = None,
+    embeddings_data = None, update_freq=160)
+  if os.path.exists(args.logs_dir):
+    model = regnet(pretrained_weights=args.logs_dir)
+    print('using pretrained data')
+  else:
+    model = regnet()
+    if not args.use_pfile:
+      model.fit_generator(myGene,steps_per_epoch=300,epochs=args.num_epochs, validation_data=myValGene, validation_steps=50, callbacks=[model_checkpoint, tb])
+    else:
+      model.fit_generator(myGene,steps_per_epoch=300,epochs=num_epochs, validation_data=myValGene, validation_steps=50, callbacks=[model_checkpoint, tb])
+
+elif args.mode == 'test':
+  if not args.use_pfile:
+    nImages = len(os.listdir(os.path.join(args.test_path, args.image_folder)))
+  else:
+    nImages = len(os.listdir(os.path.join(test_path, image_folder)))
+  if not args.use_pfile:
+    myGene = trainGenerator(args.batch_size, args.test_path, args.image_folder,
+                            args.mask_folder,
+                            args.heatmap_folder, data_gen_args, image_color_mode="rgb",
+                            mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                            heatmap_save_prefix="heatmap",
+                            flag_multi_class=True, num_class=args.class_num, save_to_dir=None,
+                            target_size=(args.h, args.w), seed=1)
+  else:
+    myGene = trainGenerator(batch_size, test_path, image_folder, mask_folder, heatmap_folder, data_gen_args, image_color_mode="rgb",
+                     mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask", heatmap_save_prefix="heatmap",
+                     flag_multi_class=True, num_class=class_num, save_to_dir=None, target_size=target_size, seed=1)
+
+  try:
+    model = regnet(pretrained_weights=args.logs_dir)
+  except:
+    raise ValueError('No model at specified dir')
+  model_checkpoint = ModelCheckpoint(args.logs_dir, monitor='loss', verbose=1, save_best_only=True)
+  eval = model.evaluate_generator(myGene, verbose=1, workers=1, steps=nImages)
+  print(eval)
+
+else:
+  if not args.use_pfile:
+    nImages = len(os.listdir(os.path.join(args.test_path, args.image_folder)))
+  else:
+    nImages = len(os.listdir(os.path.join(test_path, image_folder)))
+  print(nImages)
+  if not args.use_pfile:
+    myGene = testGenerator(1, args.test_path, args.image_folder)
+  else:
+    myGene = testGenerator(1, test_path, image_folder)
+  model = regnet()
+  try:
+    model.load_weights(args.logs_dir)
+  except:
+    raise ValueError('No model at specified dir')
+  output = model.predict_generator(myGene, workers=1, steps=nImages)
+  save_images(output, args.save_dir)
--- model/net.py
+++ model/net.py
@@ -93,3 +93,66 @@ def unet(pretrained_weights=None, input_size=(256, 256, 3), num_classes=2, seg_o
     model.load_weights(pretrained_weights)
 
   return model
+
+
+def regnet(pretrained_weights=None, input_size=(256, 256, 3), layers_trainable=True):
+  inputs = Input(input_size, name='input')
+
+  # Conv part
+  conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(inputs)
+  conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv1)
+  pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+  conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(pool1)
+  conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv2)
+  pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+  conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(pool2)
+  conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv3)
+  pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+  conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(pool3)
+  conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv4)
+  if layers_trainable:
+    drop4 = Dropout(0.5)(conv4)
+  else:
+    drop4 = Dropout(0)(conv4)
+  pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+  conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(pool4)
+  conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv5)
+  if layers_trainable:
+    drop5 = Dropout(0.5)(conv5)
+  else:
+    drop5 = Dropout(0)(conv5)
+
+  # Branch1 -- Transposed conv for segmentation
+  up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(UpSampling2D(size=(2, 2))(drop5))
+  merge6 = concatenate([drop4, up6], axis=3)
+  conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(merge6)
+  conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv6)
+  up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(UpSampling2D(size=(2, 2))(conv6))
+  merge7 = concatenate([conv3, up7], axis=3)
+  conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(merge7)
+  conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv7)
+  up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(UpSampling2D(size=(2, 2))(conv7))
+  merge8 = concatenate([conv2, up8], axis=3)
+  conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(merge8)
+  conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv8)
+  up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(UpSampling2D(size=(2, 2))(conv8))
+  merge9 = concatenate([conv1, up9], axis=3)
+  conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(merge9)
+  conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal', trainable=layers_trainable)(conv9)
+  # Multiclass segmentation output
+  conv9 = Conv2D(1, 3, activation='linear', padding='same', kernel_initializer='he_normal', name='heatmap', trainable=layers_trainable)(conv9)
+
+  model = Model(inputs=inputs, output=[conv9])
+  # model = Model(inputs=inputs, output=conv9)
+  # model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy',
+  #               metrics=['accuracy'])
+  model.compile(optimizer=Adam(lr=1e-4), loss={'heatmap':weighted_L2},
+              metrics={'heatmap': hm_recall})
+
+  # model.summary()
+
+  if (pretrained_weights) and os.path.exists(pretrained_weights):
+    print('loading weights '+pretrained_weights)
+    model.load_weights(pretrained_weights)
+
+  return model
